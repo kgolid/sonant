2512836581c8571b1903d604243ab3431500e65c
Kjetil Midtgarden Golid <kjetil.golid@gmail.com>
2020-01-14 23:50:06 +0100

    add baseurl back

--- dist/index.js
+++ dist/index.js
@@ -1098,8 +1098,8 @@
 	const melody_instrument = 'xylophone';
 
 	var synth = SampleLibrary.load({
-	  instruments: [chord_instrument, melody_instrument]
-	  //baseUrl: '/sonant/samples/' // Remove this line if working locally.
+	  instruments: [chord_instrument, melody_instrument],
+	  baseUrl: '/sonant/samples/' // Remove this line if working locally.
 	});
 
 	synth[chord_instrument].volume.value = -20;
